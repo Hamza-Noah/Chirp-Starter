85527c324059836158436054ebe2c98192d83ef1
Hamza-Noah <hamza.nouh97@gmail.com>
2022-07-07 11:40:57 +0200

    added editions to the style of graph

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,4 +1,10 @@
 const ctx = document.getElementById("myChart").getContext("2d");
+const gradientBg = ctx.createLinearGradient(0, 10, 0, 20);
+// const gradientFill = ctx.createLinearGradient(0, 10, 0, 20);
+
+gradientBg.addColorStop(0, "rgba(29, 161, 242, 0)");
+gradientBg.addColorStop(.5, "rgba(29, 161, 242, .1)");
+gradientBg.addColorStop(1, "rgba(29, 161, 242, .2)");
 
 const myChart = new Chart(ctx, {
   type: "line",
@@ -6,16 +12,30 @@ const myChart = new Chart(ctx, {
     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
     datasets: [
       {
+        type: "bar",
+        label: "Bar Dataset",
+        data: [10, 25, 20, 40],
+        borderRadius: 10,
+        backgroundColor: [
+          "transparent",
+          gradientBg,
+          "transparent",
+          "transparent",
+        ],
+      },
+      {
         label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: "#57e0e0",
+        data: [7, 19, 10, 14, 8, 22, 30],
+        backgroundColor: "rgba(87, 224, 224, .2)",
         borderColor: "#57e0e0",
         borderWidth: 1,
         tension: 0.4,
+        fill: true,
       },
     ],
   },
   options: {
+    maintainAspectRatio: false,
     plugins: {
       legend: false,
     },
@@ -51,13 +71,6 @@ const myChart = new Chart(ctx, {
   },
 });
 
-document.querySelectorAll(".unclickable").forEach((element) =>
-  element.addEventListener("click", (event) => {
-    event.preventDefault();
-    return event.stopPropagation();
-  })
-);
-
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     loop: false,
@@ -69,10 +82,12 @@ $(document).ready(function () {
       0: {
         items: 1,
         autoplay: true,
+        dots: true,
       },
       768: {
         items: 2,
         autoplay: true,
+        dots: true,
       },
       1000: {
         items: 3,
